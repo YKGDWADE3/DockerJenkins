5c422c79382ab7d058585237cd581ab486484f3c
YKGDWADE3 <superwadeg3@gmail.com>
2018-05-14 00:29:40 +0800

    first commit

--- src/main/java/com/hello/conroller/HelloController.java
+++ src/main/java/com/hello/conroller/HelloController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public ResponseEntity<String> string() {
         return new ResponseEntity<String>("hello", HttpStatus.OK);
